1210f7861550b16e4f077af501d3c661e8f648f9
Adi <kindilhome@gmail.com>
2025-04-08 12:19:13 +0300

    obstacle pic

--- HealthBar.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from Character import Character as char
-class HealthBar(char):
-    def __init__(self, player):
-        self.player = player
-    def draw(self, screen):
-        pygame.draw.rect(screen, (255, 0, 0), (20, 20, 200, 20))
-        pygame.draw.rect(screen, (0, 255, 0), (20, 20, 2 * self.player.health, 20))
-#moved to main cz why make those 2 lines so complicated
